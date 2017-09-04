675914881b4387a17493d9b583a2553ee9f85f2f
Sergei Egorov <sergei@uvita.eu>
2017-09-04 21:18:22 +0200

    fix first step

--- examples/docs/src/index.js
+++ examples/docs/src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import Step1 from "./usage-with-react/step-1"
+
 const BasicExample = () =>
   <Router>
     <div>
-      <Route exact path="/usage-with-react/step-1" component={require("./usage-with-react/step-1").default} />
+      <Route exact path="/usage-with-react/step-1" component={<Step1 search={() => {}} />} />
       <Route exact path="/usage-with-react/step-2" component={require("./usage-with-react/step-2").default} />
       <Route exact path="/usage-with-react/step-3" component={require("./usage-with-react/step-3").default} />
       <Route exact path="/usage-with-react/step-4" component={require("./usage-with-react/step-4").default} />
